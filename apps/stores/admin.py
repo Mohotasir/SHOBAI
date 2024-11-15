bae0d0cf7db798e7f418c31ed8afdd80d5e6fb83
Mohiuddin Ahmed <96897967+mohiuddinahmed121@users.noreply.github.com>
2024-11-15 16:57:28 +0600

    added stores models

--- apps/stores/admin.py
+++ apps/stores/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Store, StoreFollow, Collection
 
 # Register your models here.
+admin.site.register(Store)
+admin.site.register(StoreFollow)
+admin.site.register(Collection)
