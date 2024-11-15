b6821cdc182f86efea01ec9adb756bf4728fd48c
ShahriarArefin <arfin17310@gmail.com>
2024-11-15 17:02:09 +0600

    resolved merge stores

--- apps/stores/admin copy.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Store, StoreFollow, Collection
-
-# Register your models here.
-admin.site.register(Store)
-admin.site.register(StoreFollow)
-admin.site.register(Collection)
