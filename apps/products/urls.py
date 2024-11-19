7f55a13bbe08dc0d7444fd156c024383c9a0330e
mohotasir <mohotasir1036@gmail.com>
2024-11-19 23:36:33 +0600

    added product model - added product model - added add product view

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('',views.all_products,name='all_products'),
-    path('product_details/',views.product_details,name='product_details')
+    path("", views.all_products, name="all_products"),
+    path("<int:id>/", views.product_details, name="product_details"),
+    path("add/", views.add_product, name="add_product"),
 ]
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def all_products(request):
-    return render(request, 'all_products.html')
+    return render(request, "all_products.html")
+
+
 def product_details(request):
-    return render(request,'product_details.html')
+    return render(request, "product_details.html")
+
+
+def add_product(request):
+    return render(request, "add_product.html")
