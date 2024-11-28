41cd9638e60bdc5317d21e81d0c17bc062c0b806
ShahriarArefin <arfin17310@gmail.com>
2024-11-29 02:18:10 +0600

    feature: store follow

--- apps/stores/urls.py
+++ apps/stores/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("stores/create", views.create_store, name="create-store"),
     path("stores/<slug:slug>", views.store, name="store"),
+    path("stores/<slug:slug>/follow", views.toggle_follow_store, name="toggle-follow-store"),
     path("manage-inventory", views.manage_inventory, name="manage-inventory"),
     path("manage-orders", views.manage_orders, name="manage-orders"),
 ]
--- apps/stores/views.py
+++ apps/stores/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from apps.users.decorators import role_required
 from .forms import CreateStoreForm
-from .models import Store
+from .models import Store, StoreFollow
 
 
 # Create your views here.
@@ -9,7 +9,12 @@ def store(request, slug):
     try:
         store = Store.objects.get(slug=slug)
         collections = store.collections.all().prefetch_related("products")
-        return render(request, "store.html", {"store": store, "collections": collections})
+        is_following = StoreFollow.objects.filter(user=request.user, store=store).exists()
+        return render(
+            request,
+            "store.html",
+            {"store": store, "collections": collections, "is_following": is_following},
+        )
     except Store.DoesNotExist:
         return redirect(request.META.get("HTTP_REFERER", "home"))
 
@@ -25,15 +30,26 @@ def manage_orders(request):
 @role_required(["MERCHANT"])
 def create_store(request):
     if request.method == "POST":
-        print(request.POST)
         form = CreateStoreForm(request.POST, request.FILES)
         if form.is_valid():
             store = form.save(commit=False)
             store.merchant = request.user
             store.save()
             return redirect("dashboard")
-        else:
-            print(form.errors)
     else:
         form = CreateStoreForm()
     return render(request, "create_store.html", {"form": form})
+
+
+@role_required(["USER", "MERCHANT"], redirect_url="CURRENT")
+def toggle_follow_store(request, slug):
+    try:
+        store = Store.objects.get(slug=slug)
+        if store.merchant != request.user:
+            follow, created = StoreFollow.objects.get_or_create(user=request.user, store=store)
+            if not created:
+                follow.delete()
+    except Store.DoesNotExist:
+        pass
+
+    return redirect(request.META.get("HTTP_REFERER"))
